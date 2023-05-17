99b72189f4c71162d320c444c78cb6129a789344
Eugene Syrotiuk <eugenesyuk@gmail.com>
2023-05-17 22:38:22 +0200

    Implemented options filtering by provided value

--- src/components/Combobox.tsx
+++ src/components/Combobox.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import './Combobox.scss';
 
 type FruitOption = {
@@ -16,11 +16,27 @@ export const Combobox: FC<ComboboxProps> = ({
   data = [],
 }) => {
   const [optionsArray, setOptionsArray] = useState(data);
-  const [showPopover, setShowPopover] = useState(true);
+  const [showPopover, setShowPopover] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  
+  const filterOptions = (term: string) => {
+    const filtered = data.filter((item) => {
+      const itemValue = item.value.toLowerCase();
+      return itemValue.includes(term.toLowerCase());
+    });
+
+    setOptionsArray(filtered);
+    setShowPopover(filtered.length ? true : false);
+  };
+
+  const onInputChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.currentTarget.value);
+    filterOptions(event.currentTarget.value);
+  };
 
   return (
     <div className='combobox'>
-      <input type='text' placeholder={placeholder} className='combobox-input' />
+      <input type='text' value={inputValue} placeholder={placeholder} className='combobox-input' onChange={onInputChanged} />
       {showPopover && (
         <ul className="combobox-popover">
           {optionsArray.map((item, index) => (
